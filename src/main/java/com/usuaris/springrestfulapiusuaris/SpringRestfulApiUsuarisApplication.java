2ca9b548844ccb84f63e8617e3f03766dcbc8fcd
BernatIV <marc.mane.duatis@gmail.com>
2018-06-28 17:21:12 +0200

    First commit

---
+++ src/main/java/com/usuaris/springrestfulapiusuaris/SpringRestfulApiUsuarisApplication.java
@@ -0,0 +1,12 @@
+package com.usuaris.springrestfulapiusuaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulApiUsuarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulApiUsuarisApplication.class, args);
+	}
+}
